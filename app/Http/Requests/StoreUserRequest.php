c1a4f36c6f7e7805da5ca9a3a0eeb2450acee05a
ShadrachAroni <126606301+ShadrachAroni@users.noreply.github.com>
2024-06-10 13:53:08 +0300

    new

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class StoreUserRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'name'     => [
+                'string',
+                'required',
+            ],
+            'email'    => [
+                'required',
+                'unique:users',
+            ],
+            'phone'    => [
+                'required',
+                'unique:users',
+            ],
+            'address'    => [
+                'string',
+                'required',
+            ],
+            'password' => [
+                'required',
+            ],
+            'roles.*'  => [
+                'integer',
+            ],
+            'roles'    => [
+                'required',
+                'array',
+            ],
+        ];
+    }
+
+    public function authorize()
+    {
+        return Gate::allows('user_access');
+    }
+}
