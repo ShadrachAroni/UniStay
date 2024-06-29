469f776b3a5f6e14eb653a16691ab5f50af4e8ae
ShadrachAroni <126606301+ShadrachAroni@users.noreply.github.com>
2024-06-29 12:49:59 +0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,7 @@
      <link rel="preconnect"
      href="https://fonts.gstatic.com" crossorigin>
      <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700;800;900&display=swap"  rel="stylesheet">
+     
  
      <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 </head>
@@ -253,7 +254,71 @@
 </section>
 </div>
 
-
+<!-- Agent Registration Modal -->
+<div class="modal fade" id="create" tabindex="-1" aria-labelledby="createTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createTitle">Add User</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="createForm" method="post" action=" ">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="Fname" class="form-label">First Name</label>
+                        <input id="Fname" class="form-control" name="Fname" type="text" value="{{ old('Fname', '') }}">
+                        @error('name')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="Lname" class="form-label">Last Name</label>
+                        <input id="Lname" class="form-control" name="Lname" type="text" value="{{ old('Lname', '') }}">
+                        @error('name')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input id="email" class="form-control" name="email" type="email" value="{{ old('email', '') }}">
+                        @error('email')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="phone" class="form-label">Contact</label>
+                        <input id="phone" class="form-control" name="phone" type="text" value="{{ old('phone', '') }}">
+                        @error('phone')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Address</label>
+                        <input id="address" class="form-control" name="address" type="text" value="{{ old('address', '') }}">
+                        @error('address')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input id="password" class="form-control" name="password" type="password" value="{{ old('password', '') }}">
+                        @error('password')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Add</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Modal -->
+ 
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 
 
@@ -264,6 +329,39 @@
         document.getElementById('logout-form').submit();
     }
 </script>
+	<!-- core:js -->
+	<script src="../backend/assets/vendors/core/core.js"></script>
+	<!-- endinject -->
 
+	<!-- Plugin js for this page -->
+  <script src="../backend/assets/vendors/flatpickr/flatpickr.min.js"></script>
+  <script src="../backend/assets/vendors/apexcharts/apexcharts.min.js"></script>
+	<!-- End plugin js for this page -->
+
+	<!-- inject:js -->
+	<script src="../backend/assets/vendors/feather-icons/feather.min.js"></script>
+	<script src="../backend/assets/js/template.js"></script>
+	<!-- endinject -->
+
+	<!-- Custom js for this page -->
+  <script src="../backend/assets/js/dashboard-dark.js"></script>
+	<!-- End custom js for this page -->
+
+    <!-- Toastr JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+    @if (session('showModal'))
+        $('#create').modal('show');
+    @endif
+});
+
+$(document).ready(function() {
+    @if (session('showRegister'))
+        $('#register').modal('show');
+    @endif
+});
+    </script>
 </body>
 </html>
